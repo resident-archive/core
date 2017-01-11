3a78a5c1344f9ad9d1dd093205897d7db87f920c
Stephane Bruckert <contact@stephanebruckert.com>
2017-01-11 22:09:07 +0000

    Add items to DynamoDB

--- ra-tracks/helloworld/code.py
+++ ra-tracks/helloworld/code.py
@@ -1,9 +1,34 @@
-from __future__ import print_function
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
+from botocore.exceptions import ClientError
 import json
+import decimal
+import os
+import time
 
 
-def handler(event, context):
-    print("value1 = " + event['key1'])
-    print("value2 = " + event['key2'])
-    print("value3 = " + event['key3'])
-    return event['key1']  # Echo back the first key value
+# Helper class to convert a DynamoDB item to JSON.
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
+
+
+def lambda_handler(event, context):
+    dynamodb = boto3.resource("dynamodb", region_name='eu-west-1')
+    table = dynamodb.Table('tracks')
+
+    response = table.put_item(
+       Item={
+            'id': int(os.environ['last']) + 1,
+            'added': int(time.time())
+        }
+    )
+
+    os.environ['last'] = str(int(os.environ['last']) + 1)
+    print("PutItem succeeded:")
+    return json.dumps(response, indent=4, cls=DecimalEncoder)
