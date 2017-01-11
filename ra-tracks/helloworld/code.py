bb78214a20823747c279500318aea6c4ae64f932
Stephane Bruckert <contact@stephanebruckert.com>
2017-01-11 21:45:06 +0000

    Generated project with Gordon

---
+++ ra-tracks/helloworld/code.py
@@ -0,0 +1,9 @@
+from __future__ import print_function
+import json
+
+
+def handler(event, context):
+    print("value1 = " + event['key1'])
+    print("value2 = " + event['key2'])
+    print("value3 = " + event['key3'])
+    return event['key1']  # Echo back the first key value
