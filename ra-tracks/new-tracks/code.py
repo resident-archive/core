d33c06d0a517c102ab775c4efcd66c7aab997959
Stephane Bruckert <contact@stephanebruckert.com>
2017-01-15 17:56:48 +0000

    Get and store Spotify URI when available

--- ra-tracks/new-tracks/code.py
+++ ra-tracks/new-tracks/code.py
@@ -10,6 +10,9 @@ import time
 from bs4 import BeautifulSoup
 import urlparse
 import requests
+import spotipy
+
+spotify = spotipy.Spotify()
 
 
 def url_at_index(index):
@@ -53,25 +56,40 @@ class DecimalEncoder(json.JSONEncoder):
         return super(DecimalEncoder, self).default(o)
 
 
+def find_on_spotify(ra_name):
+    results = spotify.search(ra_name, limit=1, type='track')
+    for i, t in enumerate(results['tracks']['items']):
+        return t['uri']
+
+
 def handler(event, context):
     dynamodb = boto3.resource("dynamodb", region_name='eu-west-1')
     table = dynamodb.Table('tracks')
 
-    current_id = int(event.get('last') or os.environ.get('last'))
+    begin_time = int(time.time())
     now = int(time.time())
-    print current_id
-    ra_name = get_song_with_index(current_id)
-    print ra_name
-    response = table.put_item(
-       Item={
-            'id': current_id,
-            'added': now,
-            'ra_name': ra_name
-        }
-    )
+    while now < begin_time + 40:
+        current_id = int(os.environ.get('last') or event.get('last'))
+        try:
+            ra_name = get_song_with_index(current_id)
+        except Exception:
+            continue
 
-    os.environ['last'] = str(current_id + 1)
-    print("PutItem succeeded:")
+        spotify_uri = find_on_spotify(ra_name)
+
+        print "%s - %s | %s" % (current_id, ra_name, spotify_uri)
+
+        response = table.put_item(
+           Item={
+                'id': current_id,
+                'added': now,
+                'ra_name': ra_name,
+                'spotify': spotify_uri
+            }
+        )
+        now = int(time.time())
+        os.environ['last'] = str(current_id + 1)
+
     return json.dumps(response, indent=4, cls=DecimalEncoder)
 
 
